35ccb34bc6d916cc4caa149356841d97153d47d7
David Rojas J. <d.rojasj2@uniandes.edu.co>
2025-11-01 17:06:51 -0500

    ✨ Feat (Vehiculos): funcionalidad de listar vehiculos y mockup aplicado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ListarVehiculosComponent } from './vehiculos/listar-vehiculos/listar-vehiculos.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ListarVehiculosComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'parcial01';
--- src/app/vehiculos/listar-vehiculos/listar-vehiculos.component.ts
+++ src/app/vehiculos/listar-vehiculos/listar-vehiculos.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { VehiculosService } from '../vehiculos.service';
+import { Vehiculo } from '../vehiculo';
 
 @Component({
   selector: 'app-listar-vehiculos',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './listar-vehiculos.component.html',
-  styleUrl: './listar-vehiculos.component.scss'
+  styleUrls: ['./listar-vehiculos.component.scss']
 })
-export class ListarVehiculosComponent {
+export class ListarVehiculosComponent implements OnInit {
+  vehiculos: Vehiculo[] = [];
 
+  constructor(private vehiculosService: VehiculosService) {}
+
+  ngOnInit(): void {
+    this.vehiculosService.getVehiculos().subscribe(data => {
+      this.vehiculos = data;
+    });
+  }
 }
